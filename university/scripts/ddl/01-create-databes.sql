f898f7e5e7f087e3075cab402cadeb41020774f6
Daniel-Pinzon-Up <danipinqui1@gmail.com>
2026-05-15 08:32:13 -0500

    Add files via upload

---
+++ university/scripts/ddl/01-create-databes.sql
@@ -0,0 +1,7 @@
+CREATE USER univ_admin WITH PASSWORD 'xl.2026upbda!!';
+CREATE DATABASE universitydb WITH 
+    ENCODING='UTF8' 
+    LC_COLLATE='es_CO.UTF-8' 
+    LC_CTYPE='es_CO.UTF-8' 
+    TEMPLATE=template0 
+    OWNER = univ_admin;
